277b4e0271a7bf7000ae6c71cd5f855d17c72153
Fernando Ferraez-Rincon <167939809+cachito2002@users.noreply.github.com>
2025-10-24 10:39:27 -0600

    added another file

--- R/test 1/my_data.r
+++ R/test 1/my_data.r
@@ -1,10 +1,10 @@
-# setwd("/Users/fernandoferraez/Desktop/appliedprog/R/test 1/")
+setwd("/Users/fernandoferraez/Desktop/appliedprog/R/test 1/")
 data <- read.csv("friends.csv")
 
 library(tidyverse)
 
 under_25 <- data %>%
-  filter(age < 25)
+  filter(age == 19)
 
 print(under_25)
 
