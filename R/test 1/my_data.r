bf6144ae95ac947c8491d31a3a507db0d3d05a4d
Fernando Ferraez-Rincon <167939809+cachito2002@users.noreply.github.com>
2025-10-27 15:45:16 -0600

    added data

--- R/test 1/my_data.r
+++ R/test 1/my_data.r
@@ -1,10 +1,20 @@
-setwd("/Users/fernandoferraez/Desktop/appliedprog/R/test 1/")
-data <- read.csv("friends.csv")
+# setwd("/Users/fernandoferraez/Desktop/appliedprog/R/test 1/")
+
+# --> my file is recognized as "data"
 
 library(tidyverse)
 
-under_25 <- data %>%
-  filter(age == 19)
+data <- read.csv("friends.csv")
+#------THIS PART CLEANS UP GENDER COLUMNS TO BE MORE READABLE AND MORE MANAGEABLE------
+gender <- data %>%
+mutate(
+    friends_gender = str_to_upper(gender),
 
-print(under_25)
+    friends_gender = case_when(
+        friends_gender == "M" ~ "MALE",
+        friends_gender == "F" ~ "FEMALE",
+        TRUE ~ friends_gender
+    )
+)
+# print(gender$friends_gender)
 
